c47a5b3e9c142c4972e37bc9474536c15c46fed0
Papakarlo51 <aliserzabbarov51@gmail.com>
2025-09-09 17:32:01 +0500

    я нарисовал 2 части картины в новых компонентах с респонсив и адаптив

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Greatworks from './Components/Greatworks/Greatworks'
+import UPTech from './Components/UPTech/UPTech'
 
 const App = () => {
   return (
     <div>
+      <Greatworks />
+      <UPTech />
 
     </div>
   )

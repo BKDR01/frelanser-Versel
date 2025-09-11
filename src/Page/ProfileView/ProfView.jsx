65e27382fec1b6c6264ed031201319260643d881
Ga11ant25 <islomkhayrullaev2569@gmail.com>
2025-09-11 16:22:46 +0500

    Footer

---
+++ src/Page/ProfileView/ProfView.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import logo from './../../assets/img/UtLogo.svg'
+import search from './../../assets/icons/search2.svg'
+import bell from './../../assets/icons/Bell.svg'
+import guy from './../../assets/img/Guy.svg'
+const ProfView = () => {
+  return (
+    <>
+    <header className='bg-[#2942A5]'>
+        <div className='bg-[#1F1F1F]'>
+            <nav className='mx-auto max-w-[1220px] flex items-center py-[20px] justify-between'>
+                <div className='flex gap-[34px] items-center'>
+                    <img src={logo} alt="" />
+                    <div className='relative'>
+                        <input className='max-w-[249px] bg-[#494A4D] rounded-[100px] outline-none text-white pl-[18px] py-[12px]' type="text" placeholder='Search'/>
+                        <img className='absolute left-[170px] bottom-[15px]' src={search} alt="" />
+                    </div>
+                </div>
+                <div className='flex items-center gap-[37px]'>
+                    <ul className='flex gap-[28px]'>
+                        <li className='text-[#F2F2F2] font-semibold'>Browse projects</li>
+                        <li className='text-[#F2F2F2] font-semibold'>My Jobs</li>
+                        <li className='text-[#F2F2F2] font-semibold'>Messages</li>
+                    </ul>
+                    <div className='flex items-center gap-[41px]'>
+                        <img src={bell} alt="" />
+                        <img src={guy} alt="" />
+                    </div>
+                </div>
+            </nav>
+        </div>
+        
+    </header>
+    </>
+  )
+}
+
+export default ProfView

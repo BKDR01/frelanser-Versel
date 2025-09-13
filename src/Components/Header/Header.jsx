533c4d6382934677139425ecaed57a331d6b21bb
Stilla <sunnatilla95@gmail.com>
2025-09-13 16:38:15 +0500

    full project

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -14,9 +14,7 @@ const Header = () => {
 
           <div className='text-white'>
             <h1 className=' text-[43px] max-w-[690px] font-bold max-md:text-4xl max-lg:text-5xl max-md:text-center'>Join world’s best market place</h1>
-            <div className=''>
-              <p className='max-w-[647px] text-[16px] max-md:text-center'>Find the best Talent and best works based on your skills from around the world.</p>
-            </div>
+            <p className='max-w-[647px] text-[16px] max-md:text-center'>Find the best Talent and best works based on your skills from around the world.</p>
             <div className='mt-[28px] max-md:justify-center flex gap-4 max-sm:justigy-center'>
               <button id='run' className='outline px-[36px] py-[12px] rounded-3xl'>Find Talent</button>
               <button id='run' className='outline px-[36px] py-[12px] rounded-3xl'>Find Work</button>

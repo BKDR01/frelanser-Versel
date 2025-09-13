7fe5ad649b91498b0af2f7d3ced53618422ae089
justAziz000 <azizbeksabirov368@gmail.com>
2025-09-13 16:10:51 +0500

    Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -10,18 +10,18 @@ const Header = () => {
 
 
       </div >
-      <div className='bg bg-no-repeat bg-cover max-w[1220px] mx-auto flex flex-col pt-[30px] pb-[10px] gap-2 md:flex-row items-center text-center justify-around'>
-        <div className='text-white max-w-[12200px] text-center md:text-left md:max-w-xl'>
-          <h1 className='w-[689px] font-bold text-[43px] pt-[50px] pb-[10px] sm:text-4xl md:text-5xl'>Join world’s best market place</h1>
-          <div className='text-white pb-[28px] text-sm sm:text-base flex-col'>
-            <p>Find the best Talent and best works based on your skills from around the world.</p>
+      <div className='bg flex justify-between lg:flex-row items-center max-md-text-center flex-col-reverse'>
+        <div className='text-white'>
+          <h1 className=' text-[43px] max-w-[690px] font-bold max-md:text-4xl max-lg:text-5xl max-md:text-center'>Join world’s best market place</h1>
+          <div className=''>
+            <p className='max-w-[647px] text-[16px] max-md:text-center'>Find the best Talent and best works based on your skills from around the world.</p>
           </div>
-          <div className='flex space-x-4 justify-center'>
-            <button id='run' className='w-150px border-2 border-white py-[12px] px-[32px] rounded-3xl'>Find Talent</button>
-            <button id='run' className='w-150px border-2 border-white py-[12px] px-[32px] rounded-3xl'>Find Work</button>
+          <div className='mt-[28px] max-md:justify-center flex max-sm:justigy-center'>
+            <button id='run' className='px-[36px] py-[12px] rounded-3xl'>Find Talent</button>
+            <button id='run' className='px-[36px] py-[12px] rounded-3xl'>Find Work</button>
           </div>
         </div>
-        <div className='w-full md:w-[700px] flex justify-center'>
+        <div className=''>
           <img src={imager} alt="" />
 
         </div>

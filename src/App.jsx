d8b98c4e4285c53ed61266e17577bf32026b2b24
BKDR01 <b0327866@gmail.com>
2025-09-27 14:55:55 +0500

    get

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Route, Routes } from 'react-router';
+import Home from "./Page/Home"
+import Layout from './Page/Layout.jsx';
+import PageOneSign from './Page/PageOneSign.jsx';
+import PageOneLogin from './Page/PageOneLogin.jsx';
+import SurveQuestions from './Page/SurveQuestions.jsx';
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route element={<Layout />}>
+          <Route path='/login' element={<PageOneLogin />} />
+          <Route path='/signup' element={<PageOneSign />} />
+          <Route path='/signup/:step' element={<SurveQuestions />} />
+        </Route>
+        {/* <Route path='/signup' element={ } /> */}
+
+      </Routes>
+    </>
+  )
+}
+
+export default App                
